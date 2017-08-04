917b4f44c7d16a17dce3ebccc9fa6885fe60ddbd
Tibor Sari <tibor.sari@invia.de>
2017-08-04 09:24:53 +0200

    [modify] node-init script to automatically set the PATH in different files

--- node-init.sh
+++ node-init.sh
@@ -3,11 +3,19 @@
 mkdir -p ~/.npm-global
 
 # add binary path to PATH on host system
-{
-    echo "Add the following into your .profile, .bashrc, .zshrc or .xsessionrc"; \
-    echo ""; \
-    echo '# include user npm-global directory'; \
-    echo 'if [ -d "$HOME/.npm-global/bin" ] ; then'; \
-    echo '    PATH="$HOME/.npm-global/bin:$PATH"'; \
-    echo 'fi'; \
-}
+files=(.profile .bashrc .xsessionrc)
+for file in ${files[@]}
+do
+        grep -e "# include user npm-global directory" "$HOME/$file" &> /dev/null
+        if [ $? != 0 ]; then
+            echo "[*] including user npm-global directory into $HOME/$file";
+            {
+                echo ''; \
+                echo '# include user npm-global directory'; \
+                echo 'if [ -d "$HOME/.npm-global/bin" ] ; then'; \
+                echo '    PATH="$HOME/.npm-global/bin:$PATH"'; \
+                echo 'fi'; \
+                echo ''; \
+            } | tee -a "$HOME/$file" &> /dev/null
+        fi;
+done
